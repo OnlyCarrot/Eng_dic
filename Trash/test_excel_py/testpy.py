1b4e2cadf8a9930173de18c4065a048a3a5afd9e
Jincoco <chiomilkchio@gmail.com>
2024-04-02 17:03:29 +0900

    total excel read

--- Trash/test_excel_py/testpy.py
+++ Trash/test_excel_py/testpy.py
@@ -1,1 +1,9 @@
-#test
+import pandas as pd
+
+file_path = "C:\\Eng_dic\\Trash\\test_excel_py\\pytest.xlsx"
+
+data1 = pd.read_excel(file_path, sheet_name='과일', index_col='id')
+data2 = pd.read_excel(file_path, sheet_name='사물', index_col='id')
+print(data1)
+print("===========================")
+print(data2)
