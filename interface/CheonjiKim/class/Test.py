2a6d1fba83a81e101c59dbfa8afc7cd5a477d7b7
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2024-05-06 16:38:11 +0900

    함수 인터페이스 구현

---
+++ interface/CheonjiKim/class/Test.py
@@ -0,0 +1,16 @@
+class Test:
+
+    # userid를 통해서 유저를 식별하여 레벨테스트를 진행한다.
+    def __init__(self, userid="no_id_info"):
+        pass
+
+    def level_test(self, userid):
+        pass
+    
+    def daily_test(self, userid):
+        pass
+
+    # 프라이빗 메소드
+    # 테스트 진행하는 데 필요한 서브메소드들을 아래에 쓰자.
+
+
