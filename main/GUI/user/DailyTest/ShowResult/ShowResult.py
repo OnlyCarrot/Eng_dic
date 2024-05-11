c08072da68b1bd5671616eddf4e5548b4bcbf4cd
Jincoco <chiomilkchio@gmail.com>
2024-05-11 17:14:07 +0900

    ShowResult Add

---
+++ main/GUI/user/DailyTest/ShowResult/ShowResult.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+from tkinter import *
+from tkinter import messagebox
+"""
+from User.mainpage import MainWindow
+from Admin.admin import AdminPage
+from signup import SignupPage
+"""
+from center_window import center_window  # center_window 모듈 가져오기
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = Path(__file__).resolve().parent / "assets" / "frame"
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+class ShowResult:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("수준별 토익 영단어 프로그램")
+        self.window.geometry("1200x500")
+        self.window.configure(bg="#FFFFFF")
+
+        center_window(self.window, 1200, 500)
+
+        self.canvas = Canvas(
+            self.window,
+            bg="#FFFFFF",
+            height=500,
+            width=1200,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        self.canvas.place(x=0, y=0)
+
+        image_image = PhotoImage(
+            file=relative_to_assets("image.png"))
+        image = self.canvas.create_image(
+            600.0,
+            250.0,
+            image=image_image
+        )
+
+        image_image_1 = PhotoImage(
+            file=relative_to_assets("image-1.png"))
+        image_1 = self.canvas.create_image(
+            730.0,
+            250.0,
+            image=image_image_1
+        )
+
+        image_image_2 = PhotoImage(
+            file=relative_to_assets("image-2.png"))
+        image_2 = self.canvas.create_image(
+            130.0,
+            45.0,
+            image=image_image_2
+        )
+
+        image_image_3 = PhotoImage(
+            file=relative_to_assets("image-3.png"))
+        image_3 = self.canvas.create_image(
+            130.0,
+            140.0,
+            image=image_image_3
+        )
+
+        image_image_4 = PhotoImage(
+            file=relative_to_assets("image-4.png"))
+        image_4 = self.canvas.create_image(
+            360.0,
+            45.0,
+            image=image_image_4
+        )
+        # done 버튼
+        button_image = PhotoImage(
+            file=relative_to_assets("Button.png"))
+        temp = self.canvas.create_image(
+            750.0,
+            450.0,
+            image=button_image
+        )
+        self.window.resizable(True, True)
+        self.window.mainloop()
+
+
+
+if __name__ == "__main__":
+    ShowResult()
