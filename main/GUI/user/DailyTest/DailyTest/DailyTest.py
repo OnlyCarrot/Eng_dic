af4301d7cb08d717b3542b5e63155df0e9c2f3e3
Jincoco <chiomilkchio@gmail.com>
2024-05-21 16:54:48 +0900

    for문 적용.

--- main/GUI/user/DailyTest/DailyTest/DailyTest.py
+++ main/GUI/user/DailyTest/DailyTest/DailyTest.py
@@ -116,27 +116,7 @@ class DailyTest:
         )
         
         # TextBox
-        entry_image = PhotoImage(
-            file=relative_to_assets("TextBox.png"))
-        entry = self.canvas.create_image(
-            610.0,
-            130.0,
-            image=entry_image
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=112.3,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_1 = PhotoImage(
             file=relative_to_assets("TextBox-1.png"))
         entry_1 = self.canvas.create_image(
@@ -153,27 +133,7 @@ class DailyTest:
             image=entry_image_2
         )
 
-        entry_image_3 = PhotoImage(
-            file=relative_to_assets("TextBox-2.png"))
-        entry_3 = self.canvas.create_image(
-            950.0,
-            130.0,
-            image=entry_image_3
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=112.3,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_4 = PhotoImage(
             file=relative_to_assets("TextBox-5.png"))
         entry_4 = self.canvas.create_image(
@@ -182,27 +142,7 @@ class DailyTest:
             image=entry_image_4
         )
 
-        entry_image_5 = PhotoImage(
-            file=relative_to_assets("TextBox-4.png"))
-        entry_5 = self.canvas.create_image(
-            610.0,
-            183.5,
-            image=entry_image_5
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=165.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_6 = PhotoImage(
             file=relative_to_assets("TextBox-7.png"))
         entry_6 = self.canvas.create_image(
@@ -211,27 +151,7 @@ class DailyTest:
             image=entry_image_6
         )
 
-        entry_image_7 = PhotoImage(
-            file=relative_to_assets("TextBox-6.png"))
-        entry_7 = self.canvas.create_image(
-            950.0,
-            183.5,
-            image=entry_image_7
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=165.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_8 = PhotoImage(
             file=relative_to_assets("TextBox-9.png"))
         entry_8 = self.canvas.create_image(
@@ -240,27 +160,7 @@ class DailyTest:
             image=entry_image_8
         )
 
-        entry_image_9 = PhotoImage(
-            file=relative_to_assets("TextBox-8.png"))
-        entry_9 = self.canvas.create_image(
-            610.0,
-            237.0,
-            image=entry_image_9
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=218.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_10 = PhotoImage(
             file=relative_to_assets("TextBox-11.png"))
         entry_10 = self.canvas.create_image(
@@ -269,27 +169,7 @@ class DailyTest:
             image=entry_image_10
         )
 
-        entry_image_11 = PhotoImage(
-            file=relative_to_assets("TextBox-10.png"))
-        entry_11 = self.canvas.create_image(
-            950.0,
-            237.0,
-            image=entry_image_11
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=218.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_12 = PhotoImage(
             file=relative_to_assets("TextBox-13.png"))
         entry_12 = self.canvas.create_image(
@@ -298,27 +178,7 @@ class DailyTest:
             image=entry_image_12
         )
 
-        entry_image_13 = PhotoImage(
-            file=relative_to_assets("TextBox-12.png"))
-        entry_13 = self.canvas.create_image(
-            610.0,
-            290.5,
-            image=entry_image_13
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=273.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_14 = PhotoImage(
             file=relative_to_assets("TextBox-15.png"))
         entry_14 = self.canvas.create_image(
@@ -327,27 +187,7 @@ class DailyTest:
             image=entry_image_14
         )
 
-        entry_image_15 = PhotoImage(
-            file=relative_to_assets("TextBox-14.png"))
-        entry_15 = self.canvas.create_image(
-            950.0,
-            290.5,
-            image=entry_image_15
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=273.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_16 = PhotoImage(
             file=relative_to_assets("TextBox-17.png"))
         entry_16 = self.canvas.create_image(
@@ -356,27 +196,7 @@ class DailyTest:
             image=entry_image_16
         )
 
-        entry_image_17 = PhotoImage(
-            file=relative_to_assets("TextBox-16.png"))
-        entry_17 = self.canvas.create_image(
-            610.0,
-            344.0,
-            image=entry_image_17
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=326.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_18 = PhotoImage(
             file=relative_to_assets("TextBox-19.png"))
         entry_18 = self.canvas.create_image(
@@ -385,27 +205,42 @@ class DailyTest:
             image=entry_image_18
         )
 
-        entry_image_19 = PhotoImage(
-            file=relative_to_assets("TextBox-18.png"))
-        entry_19 = self.canvas.create_image(
-            950.0,
-            344.0,
-            image=entry_image_19
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=326.0,
-            width=130.0,
-            height=35.0
-        )
+        self.entrys = []
+
+        for i in range(0, 5):
+            self.entry = Entry(
+                self.canvas,
+                bd=0,
+                bg="#DCDCDC",
+                fg="#000716",
+                highlightthickness=0,
+                font=font
+            )
+            self.entrys.append(self.entry)
+            self.entry.place(
+                x=545.0,
+                y=113.0 + 54.5 * i,
+                width=130.0,
+                height=35.0
+            )
+        for i in range(0, 5):
+            self.entry = Entry(
+                self.canvas,
+                bd=0,
+                bg="#DCDCDC",
+                fg="#000716",
+                highlightthickness=0,
+                font=font
+            )
+            self.entrys.append(self.entry)
+
+            self.entry.place(
+                x=885.0,
+                y=113.0 + 54.5 * i,
+                width=130.0,
+                height=35.0
+            )
+
 
         #Test
         """index = Daily.daily_test(1, 10)
--- main/GUI/user/LevelTest/LevelTest2.py
+++ main/GUI/user/LevelTest/LevelTest2.py
@@ -110,27 +110,7 @@ class LevelTest2:
         )
         
         # TextBox
-        entry_image = PhotoImage(
-            file=relative_to_assets("TextBox.png"))
-        entry = self.canvas.create_image(
-            610.0,
-            130.0,
-            image=entry_image
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=112.3,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_1 = PhotoImage(
             file=relative_to_assets("TextBox-1.png"))
         entry_1 = self.canvas.create_image(
@@ -147,27 +127,7 @@ class LevelTest2:
             image=entry_image_2
         )
 
-        entry_image_3 = PhotoImage(
-            file=relative_to_assets("TextBox-2.png"))
-        entry_3 = self.canvas.create_image(
-            950.0,
-            130.0,
-            image=entry_image_3
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=112.3,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_4 = PhotoImage(
             file=relative_to_assets("TextBox-5.png"))
         entry_4 = self.canvas.create_image(
@@ -176,27 +136,7 @@ class LevelTest2:
             image=entry_image_4
         )
 
-        entry_image_5 = PhotoImage(
-            file=relative_to_assets("TextBox-4.png"))
-        entry_5 = self.canvas.create_image(
-            610.0,
-            183.5,
-            image=entry_image_5
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=165.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_6 = PhotoImage(
             file=relative_to_assets("TextBox-7.png"))
         entry_6 = self.canvas.create_image(
@@ -205,27 +145,7 @@ class LevelTest2:
             image=entry_image_6
         )
 
-        entry_image_7 = PhotoImage(
-            file=relative_to_assets("TextBox-6.png"))
-        entry_7 = self.canvas.create_image(
-            950.0,
-            183.5,
-            image=entry_image_7
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=165.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_8 = PhotoImage(
             file=relative_to_assets("TextBox-9.png"))
         entry_8 = self.canvas.create_image(
@@ -234,27 +154,7 @@ class LevelTest2:
             image=entry_image_8
         )
 
-        entry_image_9 = PhotoImage(
-            file=relative_to_assets("TextBox-8.png"))
-        entry_9 = self.canvas.create_image(
-            610.0,
-            237.0,
-            image=entry_image_9
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=218.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_10 = PhotoImage(
             file=relative_to_assets("TextBox-11.png"))
         entry_10 = self.canvas.create_image(
@@ -263,27 +163,7 @@ class LevelTest2:
             image=entry_image_10
         )
 
-        entry_image_11 = PhotoImage(
-            file=relative_to_assets("TextBox-10.png"))
-        entry_11 = self.canvas.create_image(
-            950.0,
-            237.0,
-            image=entry_image_11
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=218.5,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_12 = PhotoImage(
             file=relative_to_assets("TextBox-13.png"))
         entry_12 = self.canvas.create_image(
@@ -292,27 +172,7 @@ class LevelTest2:
             image=entry_image_12
         )
 
-        entry_image_13 = PhotoImage(
-            file=relative_to_assets("TextBox-12.png"))
-        entry_13 = self.canvas.create_image(
-            610.0,
-            290.5,
-            image=entry_image_13
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=273.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_14 = PhotoImage(
             file=relative_to_assets("TextBox-15.png"))
         entry_14 = self.canvas.create_image(
@@ -321,27 +181,7 @@ class LevelTest2:
             image=entry_image_14
         )
 
-        entry_image_15 = PhotoImage(
-            file=relative_to_assets("TextBox-14.png"))
-        entry_15 = self.canvas.create_image(
-            950.0,
-            290.5,
-            image=entry_image_15
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=273.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_16 = PhotoImage(
             file=relative_to_assets("TextBox-17.png"))
         entry_16 = self.canvas.create_image(
@@ -350,27 +190,7 @@ class LevelTest2:
             image=entry_image_16
         )
 
-        entry_image_17 = PhotoImage(
-            file=relative_to_assets("TextBox-16.png"))
-        entry_17 = self.canvas.create_image(
-            610.0,
-            344.0,
-            image=entry_image_17
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=545.0,
-            y=326.0,
-            width=130.0,
-            height=35.0
-        )
+        
         entry_image_18 = PhotoImage(
             file=relative_to_assets("TextBox-19.png"))
         entry_18 = self.canvas.create_image(
@@ -379,27 +199,42 @@ class LevelTest2:
             image=entry_image_18
         )
 
-        entry_image_19 = PhotoImage(
-            file=relative_to_assets("TextBox-18.png"))
-        entry_19 = self.canvas.create_image(
-            950.0,
-            344.0,
-            image=entry_image_19
-        )
-        self.entry = Entry(
-            self.canvas,
-            bd=0,
-            bg="#DCDCDC",
-            fg="#000716",
-            highlightthickness=0,
-            font=font
-        )
-        self.entry.place(
-            x=885.0,
-            y=326.0,
-            width=130.0,
-            height=35.0
-        )
+        self.entrys = []
+
+        for i in range(0, 5):
+            self.entry = Entry(
+                self.canvas,
+                bd=0,
+                bg="#DCDCDC",
+                fg="#000716",
+                highlightthickness=0,
+                font=font
+            )
+            self.entrys.append(self.entry)
+            self.entry.place(
+                x=545.0,
+                y=111.0 + 54.5 * i,
+                width=130.0,
+                height=35.0
+            )
+        for i in range(0, 5):
+            self.entry = Entry(
+                self.canvas,
+                bd=0,
+                bg="#DCDCDC",
+                fg="#000716",
+                highlightthickness=0,
+                font=font
+            )
+            self.entrys.append(self.entry)
+
+            self.entry.place(
+                x=885.0,
+                y=111.0 + 54.5 * i,
+                width=130.0,
+                height=35.0
+            )
+
         self.window.resizable(False, False)
         self.window.mainloop()
 
