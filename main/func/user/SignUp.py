cb6f89334f6754cfb198ae151cb33371dcc6573a
seonjiwon <tommys915@gmail.com>
2024-05-28 23:16:22 +0900

    SignUp 함수형 -> class형으로 변경

--- main/func/user/SignUp.py
+++ main/func/user/SignUp.py
@@ -6,37 +6,35 @@ from main.func.Sheet import Sheet
 from main.func.user import LevelTest
 
 
-def temp_storage(id, username, password):
-    global temp_id, temp_username, temp_password
-    temp_id = id
-    temp_username = username
-    temp_password = password
+class SignUp:
+    def temp_storage(id, username, password):
+        global temp_id, temp_username, temp_password
+        temp_id = id
+        temp_username = username
+        temp_password = password
 
-def sign_up(level):
-    # 레벨 테스트를 통해 얻은 점수를 가져옴
-    # score = LevelTest.get_total_score()
+    def sign_up(level):
+        loaded_xlsx = Sheet("usersheet")
+        sheet = loaded_xlsx.worksheet
+        workbook = loaded_xlsx.workbook
 
-    loaded_xlsx = Sheet("usersheet")
-    sheet = loaded_xlsx.worksheet
-    workbook = loaded_xlsx.workbook
+        # 마지막 행에 새로운 사용자 정보 추가
+        new_user = [temp_id, temp_password, temp_username, "user", level]
+        sheet.append(new_user)
 
-    # 마지막 행에 새로운 사용자 정보 추가
-    new_user = [temp_id, temp_password, temp_username, "user", level]
-    sheet.append(new_user)
+        # 엑셀 파일 저장
+        workbook.save("main/DB/UserList.xlsx")
 
-    # 엑셀 파일 저장
-    workbook.save("main/DB/UserList.xlsx")
+        print("추가되었습니다.")
 
-    print("추가되었습니다.")
-
-def is_str_vaild(entries):
-    for entry in entries:
-        if not entry.strip():
-            return False
-    return True
-
-def is_pw_dupli(pw1, pw2):
-    if pw1 != pw2:
+    def is_str_vaild(entries):
+        for entry in entries:
+            if not entry.strip():
+                return False
         return True
-    else:
-        return False
+
+    def is_pw_dupli(pw1, pw2):
+        if pw1 != pw2:
+            return True
+        else:
+            return False
--- main/test/user/SignUpTest.py
+++ main/test/user/SignUpTest.py
@@ -1,8 +1,8 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
-from func.user.SignUp import sign_up, temp_storage
+from func.user.SignUp import SignUp
 
 #Test: 새로운 user가 UserList에 추가 되는지 확인
-temp_storage("test", "test", "test")
-sign_up(666)
+SignUp.temp_storage("test", "test", "test")
+SignUp.sign_up(666)
